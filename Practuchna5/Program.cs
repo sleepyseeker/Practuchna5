0ab1985dd1242655b173d1961a8fd63c6d0a306a
Player <Player@PC>
2022-11-24 18:28:29 +0200

    Добавьте файлы проекта.

---
+++ Practuchna5/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Pracruchna5
+{
+    public class Counter
+    {
+        private int max;
+        private int min;
+        private int count;
+
+        public Counter(int count, int min, int max)
+        {
+            this.count = count;
+            this.min = min;
+            this.max = max;
+        }
+
+        public Counter()
+        {
+            this(0, 0, 10);
+        }
+
+        public void setMax(int max)
+        {
+            this.max = max;
+        }
+
+        public void setMin(int min)
+        {
+            this.min = min;
+        }
+
+        public void setCount(int count)
+        {
+            this.count = count;
+        }
+
+        public void up()
+        {
+            count++;
+        }
+
+        public void down()
+        {
+            count--;
+        }
+    }
+}
